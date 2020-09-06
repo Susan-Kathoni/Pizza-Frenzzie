3eef7834ec3b69c4447deb6b6a5f09ffcf78428d
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-09-06 12:35:45 +0300

    Add jquery function

---
+++ JavaScript/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    function Pizza(flavour, size, toppings, crust) {
+        this.flavour = flavour;
+        this.size = size;
+        this.toppings = toppings;
+        this.crust = crust;
+    }
+    
+})
+
