275c8086fa142e8d386ef2a05965455b1e73046e
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-09-07 07:50:00 +0300

    Add pizza flavour prototype

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -9,5 +9,31 @@ $(document).ready(function () {
         return this.getCrustPrice() + this.getToppingPrice() + this.getFlavourPrice();
       };  
 })
-
-
+//---------------------------Price:flavour--------------------->
+Pizza.prototype.getFlavourPrice = function () {
+    if (this.size === "large") {
+      if (this.getFlavourPrice === "chicken tikka") {
+        return 1200;
+      } else if (this.type === "peri peri") {
+        return 1300;
+      } else {
+        return 1100;
+      }
+    } else if (this.size === "medium") {
+      if (this.type === "chicken tikka") {
+        return 850;
+      } else if (this.type === "peri peri") {
+        return 950;
+      } else {
+        return 750;
+      }
+    } else {
+      if (this.type === "chicken tikka") {
+        return 600;
+      } else if (this.type === "peri peri") {
+        return 650;
+      } else {
+        return 550;
+      }
+    }
+  };
