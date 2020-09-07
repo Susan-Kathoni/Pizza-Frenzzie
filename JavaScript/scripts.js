854a7de4eba1e9f8839ee9f801bd519dd550d653
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-09-07 08:33:04 +0300

    Add pizza flavour prices depending on sizes

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -11,29 +11,61 @@ $(document).ready(function () {
 })
 //---------------------------Price:flavour--------------------->
 Pizza.prototype.getFlavourPrice = function () {
-    if (this.size === "large") {
-      if (this.getFlavourPrice === "chicken tikka") {
-        return 1200;
-      } else if (this.type === "peri peri") {
-        return 1300;
-      } else {
-        return 1100;
-      }
+    if (this.size === "regular") {
+        if (this.flavour === "beef-pepporoni") {
+            return 300;
+        } else if (this.flavour === "cheese-pizza") {
+            return 500;
+        } else if (this.fllavour === "chicken-hawaiian") {
+            return 400;
+        } else if (this.flavour === "macon-bbq") {
+            return 480;
+        } else if (this.flavour === "meat-deluxe") {
+            return 350;
+        } else {
+            return 320;
+        }
     } else if (this.size === "medium") {
-      if (this.type === "chicken tikka") {
-        return 850;
-      } else if (this.type === "peri peri") {
-        return 950;
-      } else {
-        return 750;
-      }
-    } else {
-      if (this.type === "chicken tikka") {
-        return 600;
-      } else if (this.type === "peri peri") {
-        return 650;
-      } else {
-        return 550;
-      }
+        if (this.flavour === "beef-pepporoni") {
+            return 500;
+        } else if (this.flavour === "cheese-pizza") {
+            return 750;
+        } else if (this.fllavour === "chicken-hawaiian") {
+            return 550;
+        } else if (this.flavour === "macon-bbq") {
+            return 560;
+        } else if (this.flavour === "meat-deluxe") {
+            return 480;
+        } else {
+            return 400;
+        }
+    } else if (this.size === "large") {
+        if (this.flavour === "beef-pepporoni") {
+            return 650;
+        } else if (this.flavour === "cheese-pizza") {
+            return 900;
+        } else if (this.fllavour === "chicken-hawaiian") {
+            return 700;
+        } else if (this.flavour === "macon-bbq") {
+            return 650;
+        } else if (this.flavour === "meat-deluxe") {
+            return 530;
+        } else {
+            return 500;
+        }
+    } else if (this.size === "etra-large") {
+        if (this.flavour === "beef-pepporoni") {
+            return 800;
+        } else if (this.flavour === "cheese-pizza") {
+            return 1100;
+        } else if (this.fllavour === "chicken-hawaiian") {
+            return 850;
+        } else if (this.flavour === "macon-bbq") {
+            return 770;
+        } else if (this.flavour === "meat-deluxe") {
+            return 650;
+        } else {
+            return 680;
+        }
     }
-  };
+};
